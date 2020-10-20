55306f72c6f6ac31afaf02828b67ad0a454e9680
ericlebot <eriklebot@gmail.com>
2020-10-20 14:12:29 +0200

    Created initial project structure

---
+++ main.js
@@ -0,0 +1,26 @@
+let assets;
+
+let game = hexi(512, 512, setup, assets, load);
+
+game.fps = 60;
+
+function play () {
+
+
+
+}
+
+function setup () {
+
+
+
+    game.state = play;
+}
+
+function load () {
+
+
+
+}
+
+game.start();
